992c3fb96e8eb1dab3ba9f21714a0422137d3a83
venkatmanavarthi <manavarthivenkat@icloud.com>
2023-10-16 20:57:21 -0500

    Initial Commit

---
+++ FilmFest2UITests/FilmFest2UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  FilmFest2UITestsLaunchTests.swift
+//  FilmFest2UITests
+//
+//  Created by Venkat Rao Manavarthi on 10/16/23.
+//
+
+import XCTest
+
+final class FilmFest2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
